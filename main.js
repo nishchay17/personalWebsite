1b128804654f291568b5f2b397e55db94da8a292
nishchay17 <nishchay13971@gmail.com>
2019-12-24 23:07:06 +0530

    added dark mode

---
+++ main.js
@@ -0,0 +1,61 @@
+let isDark = false;
+const darkBotton = document.getElementById('dark');
+const nav = document.getElementById('nav');
+const body = document.querySelector('body');
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const btn3 = document.getElementById('btn3');
+const btn4 = document.getElementById('btn4');
+const l1 = document.getElementById('l1');
+const l2 = document.getElementById('l2');
+const l3 = document.getElementById('l3');
+const l4 = document.getElementById('l4');
+const l5 = document.getElementById('l5');
+
+function toDark(){
+    body.classList.add('dark');
+    body.classList.add('text-light');
+    btn1.classList.add('btn-dark');
+    btn2.classList.add('btn-dark');
+    btn3.classList.add('btn-dark');
+    btn4.classList.add('btn-dark');
+    l1.classList.add('dark1');
+    l2.classList.add('dark1');
+    l3.classList.add('dark1');
+    l4.classList.add('dark1');
+    l5.classList.add('dark1');
+    nav.classList.remove("navbar-light", "bg-light");
+    nav.classList.add("navbar-dark", "dark1");
+    darkBotton.innerHTML = "Light Mode";
+    darkBotton.classList.remove("btn-dark");
+    darkBotton.classList.add("btn-light");
+    isDark = true;
+}
+
+function toLight(){
+    body.classList.remove('dark');
+    body.classList.remove('text-light');
+    btn1.classList.remove('btn-dark');
+    btn2.classList.remove('btn-dark');
+    btn3.classList.remove('btn-dark');
+    btn4.classList.remove('btn-dark');
+    l1.classList.remove('dark1');
+    l2.classList.remove('dark1');
+    l3.classList.remove('dark1');
+    l4.classList.remove('dark1');
+    l5.classList.remove('dark1');
+    nav.classList.remove("navbar-dark", "dark1");
+    nav.classList.add("navbar-light", "bg-light");
+    darkBotton.innerHTML = "Dark Mode";
+    darkBotton.classList.remove("btn-light");
+    darkBotton.classList.add("btn-dark");
+    isDark = false;
+}
+
+
+darkBotton.addEventListener('click', () => {
+    if(!isDark)
+        toDark();
+    else
+        toLight();
+});
